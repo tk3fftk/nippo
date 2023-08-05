ee0069c47d50ea98e94b57eb8459ed552d7359d8
tk3fftk <tktk.stereoman@gmail.com>
2023-08-05 17:17:53 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { Client, isFullPage } from "@notionhq/client";
+import {
+  CreatePageResponse,
+  createDatabase,
+} from "@notionhq/client/build/src/api-endpoints";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+  const today = new Date();
+  const todayString = `${today.getFullYear()}/${
+    today.getMonth() + 1
+  }/${today.getDate()}`;
+
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+  const databaseId = process.env.NOTION_DATABASE_ID || "";
+
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+
+  const createPageResponse: CreatePageResponse = await notion.pages.create({
+    parent: {
+      database_id: databaseId,
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: todayString,
+            },
+          },
+        ],
+      },
+      Date: {
+        type: "date",
+        date: {
+          start: today.toISOString(),
+          end: null,
+        },
+      },
+    },
+  });
+
+  if (isFullPage(createPageResponse)) {
+    console.log("today's page has been created.");
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
